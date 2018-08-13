c16bafadc0a1d6d23f557142bbff217b67194ee6
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-13 23:25:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,7 +1,5 @@
 require 'pry'
 
-def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
   # {
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],
@@ -17,33 +15,46 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+  
+
+def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
 
+def add_supply_to_winter_holidays(holiday_hash, supply)
+  #not working
+  holiday_hash[:winter][:christmas] << "#{supply}" 
+  holiday_hash[:winter][:new_years] << "#{supply}"
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << "#{supply}"
 end
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
+
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
+  holiday_supplies = []
+  
+    holiday_hash.each do |season, |
+
+  
+  holiday_supplies << holiday_hash[:winter][:christmas].flatten
+  holiday_supplies << holiday_hash[:winter][:new_years].flatten
+  
+  holiday_supplies
+
 end
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -63,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
 end
 
 
-
+end 
 
 
 
