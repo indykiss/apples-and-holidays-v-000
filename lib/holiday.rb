ff5ec722431e7a50243d4e2d242c92c8d89e5b31
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-13 23:57:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,12 +56,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash.each do |season, holiday| 
-
+  holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
-    
     holiday.each do |name, supplies|
-        
         capitalized_name = name.to_s.split('_').each do |word|
             word.capitalize!
           end
