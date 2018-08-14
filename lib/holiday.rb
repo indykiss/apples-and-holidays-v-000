cef571160c3654b181fc08f45a05a846b070f93f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-14 00:00:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
 
   holiday_hash.map do |season, holidays|
       holidays.map do |holiday, supplies|
-        answer << supplies.include?("BBQ")
+        answer[supplies] << supplies.include?("BBQ")
       end
     end
     answer
