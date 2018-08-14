fc889d18b970503b6adc18e5df2ecad4c4f2fd1c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-08-14 00:02:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,14 +48,6 @@ end
 
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}:"
     holiday.each do |name, supplies|
@@ -66,16 +58,16 @@ def all_supplies_in_holidays(holiday_hash)
           puts "  #{capitalized_name}: #{supplies.join(", ")}"
         end
       end
-    end
+  end
+
 
 def all_holidays_with_bbq(holiday_hash)
-
-  answer = []
-
   holiday_hash.map do |season, holidays|
       holidays.map do |holiday, supplies|
-        answer << supplies.include?("BBQ")
+        if supplies.include?("BBQ")
+          puts supplies 
+        else 
+          false
       end
     end
-    answer
 end
